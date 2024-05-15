5bd0fee73d007d299fe214f7fc10d2a5e2bc0c84
Karen Rubio <120537042+KarenRubio@users.noreply.github.com>
2024-05-15 17:09:40 +0000

    Models Created

--- src/api/models.py
+++ src/api/models.py
@@ -25,6 +25,8 @@ class Company(db.Model):
     name = db.Column(db.String(120) )
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
+    product = db.relationship('Product', backref='product')
+
     
 
     def __repr__(self):
@@ -36,4 +38,30 @@ class Company(db.Model):
             "name": self.name,
             "email": self.email,
             # do not serialize the password, its a security breach
+        }
+
+
+class Product(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120),nullable=False)
+    description = db.Column(db.String(120), nullable=False)
+    quantity = db.Column(db.Integer, unique=False, nullable=False)
+    price = db.Column(db.Integer, unique=False, nullable=False)
+    category_id = db.Column(db.Integer, db.ForeignKey('category.id'),nullable=False) #agregar relationship a category
+    company_id = db.Column(db.Integer, db.ForeignKey('company.id'),nullable=False)
+    
+
+    def __repr__(self):
+        return f'<Product {self.name}>'
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "description": self.email,
+            "quantity": self.quantity,
+            "price": self.price,
+            "category_id": self.category_id,
+            "company_id": self.company_id
+            
         }
--- src/api/routes.py
+++ src/api/routes.py
@@ -2,7 +2,7 @@
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
 from flask import Flask, request, jsonify, url_for, Blueprint
-from api.models import db, User,Company
+from api.models import db, User,Company,Product
 from api.utils import generate_sitemap, APIException
 from flask_cors import CORS
 
@@ -76,3 +76,70 @@ def update_company(company_id):
  
 
     return jsonify(company.serialize()), 200
+
+
+#Endpoint product
+
+@api.route('/product', methods=['GET'])
+def get_products():
+    all_products = Product.query.all()
+    results = list(map(lambda elemento: elemento.serialize() , all_products))
+
+    return jsonify(results), 200
+
+
+@api.route('/product/<int:product_id>', methods=['GET'])
+def get_product(product_id):
+    one_product = Product.query.filter_by(id=product_id).first()
+
+    return jsonify(one_product.serialize()), 200
+
+
+@api.route('/product', methods=['POST'])
+def add_product():
+    body= request.get_json()
+    new_product = Product(
+        name=body['name'],
+        description=body['description'],
+        quantity=body['quantity'],
+        price=body['price'],
+        category_id=body['category_id'],
+        company_id=body['company_id'],
+    )
+    db.session.add(new_product)
+    db.session.commit()
+
+    return jsonify(new_product.serialize()), 201
+
+@api.route('/product/<int:product_id>', methods=['DELETE'])
+def delete_product(product_id):
+    one_product = Product.query.filter_by(id=product_id).first()
+    db.session.delete(one_product)
+    db.session.commit()
+    response_body = {
+        "msg": "Product eliminado correctamente",
+        "product_id": product_id,
+        
+    }
+
+    return jsonify(response_body), 200
+
+@api.route('/product/<int:product_id>', methods=['PUT'])
+def update_product(product_id):
+    product = Product.query.filter_by(id=product_id).first()
+    body= request.get_json()
+    if 'name' in body:
+        product.name = body['name']
+    if 'description' in body:
+        product.description = body['description']
+    if 'quantity' in body:
+        product.quantity = body['quantity']
+    if 'price' in body:
+        product.price = body['price']
+    if 'category_id' in body:
+        product.category_id = body['category_id']
+    
+    db.session.commit()
+ 
+
+    return jsonify(product.serialize()), 200
